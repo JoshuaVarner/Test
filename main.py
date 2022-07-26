3152d29b52172ed4d6b3368a8e05c0e8b08061d5
JoshuaVarner <110063334+JoshuaVarner@users.noreply.github.com>
2022-07-26 14:05:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-num = int (input (“Enter any number:“)
-if (num % 2) == 0:
-  print (“The number is even”)
-else:
-    print (“The provided number is odd”)
+while True:
+    try:    
+        num = float(input("Enter a number: "))
+    except ValueError:
+        print("input is not a number")
+        continue
+    else:
+        if (num % 2) == 0:
+            print(str(num) + " is Even")
+        else:
+            print(str(num) + " is Odd")
